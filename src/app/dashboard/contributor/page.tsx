73b45566e0d36c90804e030712b85c2cb9136d53
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-08-20 23:42:00 -0400

    (master) format files

--- src/app/dashboard/contributor/page.tsx
+++ src/app/dashboard/contributor/page.tsx
@@ -1,14 +1,13 @@
 export default function Page() {
-    return (
-      <>
-        <h1>contribuer</h1>
-        <ul className="list">
-          <li>making webviews work for the web</li>
-          <li>remote meeting agenda in development</li>
-          <li>w3c accessibility maturity model</li>
-          <li>stepping forward on wai management</li>
-        </ul>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <h1>contribuer</h1>
+      <ul className="list">
+        <li>making webviews work for the web</li>
+        <li>remote meeting agenda in development</li>
+        <li>w3c accessibility maturity model</li>
+        <li>stepping forward on wai management</li>
+      </ul>
+    </>
+  );
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,32 +1,48 @@
 import "./style.css";
 import type { ReactNode } from "react";
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="main" style={{backgroundColor: 'black', height: "100vh", maxWidth: "100%"}}>
+    <main
+      className="main"
+      style={{ backgroundColor: "black", height: "100vh", maxWidth: "100%" }}
+    >
       <div className="wrapper">
-      <div style={{fontFamily: "Hyperion", fontSize: 20, color: "black", display: "flex"}}>
-        GITBOUNTIES
+        <div
+          style={{
+            fontFamily: "Hyperion",
+            fontSize: 20,
+            color: "black",
+            display: "flex",
+          }}
+        >
+          GITBOUNTIES
         </div>
-        <div style={{display: "flex"}}>
-          <Link href="/dashboard/maintainer" className="relative inline-block px-4 py-2 font-medium group">
-<span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-<span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-<span className="relative text-black group-hover:text-white">Maintainer</span>
-</Link>
-<Link href="/dashboard/contributor" className="relative inline-block px-4 py-2 font-medium group">
-<span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-<span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
-<span className="relative text-black group-hover:text-white">Contributor</span>
-</Link>
+        <div style={{ display: "flex" }}>
+          <Link
+            href="/dashboard/maintainer"
+            className="relative inline-block px-4 py-2 font-medium group"
+          >
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+            <span className="relative text-black group-hover:text-white">
+              Maintainer
+            </span>
+          </Link>
+          <Link
+            href="/dashboard/contributor"
+            className="relative inline-block px-4 py-2 font-medium group"
+          >
+            <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+            <span className="relative text-black group-hover:text-white">
+              Contributor
+            </span>
+          </Link>
         </div>
         <div>{children}</div>
-        </div>
+      </div>
     </main>
   );
 }
