ae5386328bdc9475bb59294f1db69a5c73c4b856
Yicheng Zhang <zychelenzhang@gmail.com>
2023-08-21 20:52:25 -0400

    add popup for convert into a bounty

--- src/app/dashboard/maintainer/page.tsx
+++ src/app/dashboard/maintainer/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 export default function Page() {
   const issueItem = (
@@ -55,12 +57,52 @@ export default function Page() {
         </td>
         {status && (
           <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-            <a
-              href="maintainer/bounty"
-              className="text-indigo-600 hover:text-indigo-900 focus:outline-none focus:underline"
+            <Popup
+              trigger={
+                <button className="text-indigo-600 hover:text-indigo-900 focus:outline-none">
+                  Convert into a bounty
+                </button>
+              }
+              position={"right center"}
+              modal
+              nested
             >
-              Convert into a bounty
-            </a>
+              <>
+                <div style={{ display: "table", width: "100%" }}>
+                  <label
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    style={{
+                      display: "table-cell",
+                      whiteSpace: "nowrap",
+                      width: "1px",
+                    }}
+                  >
+                    How much money do you want to put in?
+                  </label>
+                  <span
+                    style={{ display: "table-cell", padding: "0 4px 0 6px" }}
+                  >
+                    <input
+                      type="text"
+                      id="first_name"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                      style={{ width: "30%", marginLeft: "2%" }}
+                    ></input>
+                  </span>
+                </div>
+                <div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Gas Fee:{" "}
+                </div>
+                <button
+                  type="submit"
+                  className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none"
+                  style={{ marginLeft: "45%" }}
+                >
+                  Submit
+                </button>
+              </>
+            </Popup>
           </td>
         )}
       </tr>
