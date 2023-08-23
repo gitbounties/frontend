f713a6fc90fff8952d06e95252377198cd5c934b
Yicheng Zhang <zychelenzhang@gmail.com>
2023-08-23 17:12:02 -0400

    make login and register button the same size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,11 @@ export default function Home() {
       <button
         type="button"
         className="text-white bg-gradient-to-br  shadow-xl  from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        style={{ display: "flex", alignItems: "center" }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          width: "15%",
+        }}
         onClick={() => {
           window.location.href = GITHUB_REGISTER_URL;
         }}
@@ -124,7 +128,12 @@ export default function Home() {
       <button
         type="button"
         className="text-white bg-gradient-to-br  shadow-xl  from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-        style={{ marginBottom: "20vh", display: "flex", alignItems: "center" }}
+        style={{
+          marginBottom: "20vh",
+          display: "flex",
+          alignItems: "center",
+          width: "15%",
+        }}
         onClick={() => {
           window.location.href = GITHUB_LOGIN_URL;
         }}
