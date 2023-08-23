f55ea5dc2290d6641b758efd60f48e606d866293
Daniel Liu <mr.picklepinosaur@gmail.com>
2023-08-22 21:40:32 -0400

    mint nft when creating bounty

--- src/app/dashboard/maintainer/page.tsx
+++ src/app/dashboard/maintainer/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+
+import useGitbountiesContract from "@/hooks/useGitbountiesContract";
+import { Contract } from "ethers";
 import React, { useEffect, useState } from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
+import IssueItem from "./IssueItem";
 
 interface Issue {
   issue_id: number;
@@ -16,29 +20,43 @@ interface GithubIssue {
   issue: Issue;
 }
 
-function IssueItem({ item }: { item: GithubIssue }) {
-  const pfp =
-    "https://avatars.githubusercontent.com/u/45017130?s=400&u=fca9f286318fbbe286f7a8c29cc0c040402c6df7&v=4";
-  const status: "Open" | "Closed" = "Open";
+export type createBountyFn = (issue: Issue, reward: number) => Promise<void>;
 
-  const onSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ): Promise<void> => {
-    e.preventDefault();
-    const { elements } = e.currentTarget;
+const mintNFT = async (contract: Contract): Promise<number> => {
+  const tx = await contract.mint();
+  // console.log('transaction', tx);
+  const receipt = await tx.wait();
+  // console.log('transaction result', receipt);
+  const filter = contract.filters.Transfer(null, null, null);
+  const transferEvents = await contract.queryFilter(filter);
 
-    const bountyAmount = (
-      elements.namedItem("bountyAmount") as HTMLInputElement
-    ).value;
+  console.log("transferEvents", transferEvents);
+
+  const tokenId = transferEvents[transferEvents.length - 1].topics[3];
+  console.log("minted new token with tokenId", tokenId);
+
+  return parseInt(tokenId);
+};
+
+export default function Page() {
+  const [issues, setIssues] = useState<GithubIssue[]>([]);
+
+  const contract = useGitbountiesContract();
+  console.log(contract);
+
+  const createBounty = async (issue: Issue, reward: number): Promise<void> => {
+    // mint NFT for user
+    const tokenId = await mintNFT(contract);
+    console.log("minted token with id", tokenId);
 
     const data = {
-      reward: parseInt(bountyAmount),
+      reward: reward,
     };
 
     const query = new URLSearchParams({
-      owner: item.issue.owner,
-      repo: item.issue.repo,
-      issue: item.issue.issue_id.toString(),
+      owner: issue.owner,
+      repo: issue.repo,
+      issue: issue.issue_id.toString(),
     });
 
     const res = await fetch(
@@ -57,110 +75,53 @@ function IssueItem({ item }: { item: GithubIssue }) {
     console.log("result", res);
   };
 
-  return (
-    <tr>
-      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <div className="flex items-center">
-          <div className="flex-shrink-0 h-10 w-10">
-            <img className="h-10 w-10 rounded-full" src={pfp} alt="" />
-          </div>
-          <div className="ml-4">
-            <div className="text-sm leading-5 font-medium text-gray-900">
-              {item.title}
-            </div>
-            <div className="text-sm leading-5 text-gray-500">{item.author}</div>
-          </div>
-        </div>
-      </td>
-      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-        <div className="text-sm leading-5 text-gray-900">
-          {item.issue.owner}
-        </div>
-        <div className="text-sm leading-5 text-gray-500">{item.issue.repo}</div>
-      </td>
-
-      {status == "Open" && (
-        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-            Active
-          </span>
-        </td>
-      )}
-      {status == "Open" && (
-        <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-          <Popup
-            trigger={
-              <button className="text-indigo-600 hover:text-indigo-900 focus:outline-none">
-                Convert into a bounty
-              </button>
-            }
-            position={"right center"}
-            modal
-            nested
-          >
-            <form onSubmit={onSubmit}>
-              <div style={{ display: "table", width: "100%" }}>
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  style={{
-                    display: "table-cell",
-                    whiteSpace: "nowrap",
-                    width: "1px",
-                  }}
-                >
-                  How much money do you want to put in?
-                </label>
-                <span style={{ display: "table-cell", padding: "0 4px 0 6px" }}>
-                  <input
-                    type="number"
-                    id="bountyAmount"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required
-                    style={{ width: "30%", marginLeft: "2%" }}
-                  ></input>
-                </span>
-              </div>
-              <div className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                Gas Fee:{" "}
-              </div>
-              <button
-                type="submit"
-                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 bg-gray-900 rounded-lg hover:bg-gray-800 focus:shadow-outline focus:outline-none"
-                style={{ marginLeft: "45%" }}
-              >
-                Submit
-              </button>
-            </form>
-          </Popup>
-        </td>
-      )}
-    </tr>
-  );
-}
-
-export default function Page() {
-  const [issues, setIssues] = useState<GithubIssue[]>([]);
-
-  useEffect(() => {
-    const fetchIssues = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/issue`, {
-        method: "get",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+  if (process.env.NEXT_PUBLIC_USE_DUMMY_API == "true") {
+    useEffect(() => {
+      setIssues([
+        {
+          author: "MrPicklePinosaur",
+          description: "- [ ] forth\r\n- [ ] golang assembly",
+          title: "more languages",
+          issue: {
+            issue_id: 1,
+            owner: "MrPicklePinosaur",
+            repo: "advent_of_code",
+          },
         },
-        credentials: "include",
-      });
+        {
+          author: "MrPicklePinosaur",
+          description: "yet another test",
+          title: "another test",
+          issue: {
+            issue_id: 2,
+            owner: "gitbounties",
+            repo: "sample_repo",
+          },
+        },
+      ]);
+    }, []);
+  } else {
+    useEffect(() => {
+      const fetchIssues = async () => {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/issue`, {
+          method: "get",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
 
-      const body: GithubIssue[] = await res.json();
+        const body: GithubIssue[] = await res.json();
 
-      setIssues(() => body);
+        setIssues(() => body);
 
-      console.log("body", body);
-    };
+        console.log("body", body);
+      };
 
-    fetchIssues().catch(console.error);
-  }, []);
+      fetchIssues().catch(console.error);
+    }, []);
+  }
 
   return (
     <div className="flex flex-col">
@@ -183,7 +144,11 @@ export default function Page() {
             </thead>
             <tbody className="bg-white">
               {issues.map((item: GithubIssue) => (
-                <IssueItem key={item.issue.issue_id} item={item}></IssueItem>
+                <IssueItem
+                  key={item.issue.issue_id}
+                  item={item}
+                  createBounty={createBounty}
+                ></IssueItem>
               ))}
             </tbody>
           </table>
