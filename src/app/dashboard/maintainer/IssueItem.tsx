7bad1abf02a12a2a742077f6aae940b0f6d1ee9d
Maisha Thasin <thasin.maisha@gmail.com>
2023-08-23 21:05:13 -0400

    preettier

--- src/app/dashboard/maintainer/IssueItem.tsx
+++ src/app/dashboard/maintainer/IssueItem.tsx
@@ -140,7 +140,7 @@ export default function IssueItem({
               </button> */}
               <button
                 type="submit"
-                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 style={{ marginLeft: "35%", marginTop: "40%" }}
               >
                 Submit
--- src/app/onboarding/maintainer/page.tsx
+++ src/app/onboarding/maintainer/page.tsx
@@ -48,11 +48,12 @@ export default function Page() {
               type="button"
               data-modal-target="crypto-modal"
               onClick={connectWallet}
-              className="text-white bg-gradient-to-br shadow-xl  from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2"
+              className="text-white bg-gradient-to-br shadow-xl  from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-2 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-400 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2"
               style={{
                 display: "flex",
                 alignItems: "center",
                 justifyContent: "center",
+                marginLeft: "420px",
               }}
             >
               <svg
